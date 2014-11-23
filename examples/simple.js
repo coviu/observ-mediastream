c9fd763d7ea73a6f8d58703a0207198cd7202561
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-23 17:19:46 +1100

    Update example

--- examples/simple.js
+++ examples/simple.js
@@ -2,6 +2,10 @@ var ObservStream = require('..');
 var getUserMedia = require('getusermedia');
 var stream = ObservStream();
 
+stream(function(value) {
+  console.log('captured changes: ', value);
+});
+
 getUserMedia({ audio: true, video: true }, function(err, s) {
   if (err) {
     return console.error('could not capture media stream: ', err);
@@ -11,11 +15,5 @@ getUserMedia({ audio: true, video: true }, function(err, s) {
     stream.muted.set(true);
   }, 1000);
 
-  stream(function(value) {
-    console.log('captured changes: ', value);
-  });
-
-  // we've patched an update function into the observ-struct so we can
-  // supply the complex stream value
-  stream.update(s);
+  stream.set(s);
 });
